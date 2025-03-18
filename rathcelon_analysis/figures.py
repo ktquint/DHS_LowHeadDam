fb624eb42a01d0fdba88c6da5573092f63d75bb4
ktquint <ktquint@byu.edu>
2025-03-18 12:03:14 -0600

    figures.py is working

--- rathcelon_analysis/figures.py
+++ rathcelon_analysis/figures.py
@@ -1,5 +1,7 @@
+import os
 import re
 import ast
+import numpy as np
 import pandas as pd
 import dbfread as dbf
 import matplotlib.pyplot as plt
@@ -32,6 +34,10 @@ def get_attribute_df(curve_dbf):
 
 
 def get_xs_df(xs_txt):
+    """
+    this guy parses the tab-separated txt file and
+    returns the information in the form of a data frame
+    """
     xs_df = pd.read_csv(xs_txt, header=None, sep='\t')
     xs_df = xs_df.rename(columns={0: 'cell_comid', 1: 'row', 2: 'column',
                                   3: 'xs_profile1', 4: 'd_wse', 5: 'd_distance_z1', 6: "manning's_n1",
@@ -58,11 +64,11 @@ def get_within_banks(xs_df):
         xs_df.loc[i, "manning's_n2"] = xs_df.loc[i, "manning's_n2"][:index_bank2]
     return xs_df
 
+"""
+these functions plot create plots from the data frames
+"""
 
 def plot_cross_sections(attribute_df, xs_df, output_dir, in_banks):
-    # this id will be used to name the output files
-    lhd_id = attribute_df['lhd_id'][0]
-
     # if we're only plotting in banks we need to trim down the xs data frame
     if in_banks:
         xs_df = get_within_banks(xs_df)
@@ -94,20 +100,58 @@ def plot_cross_sections(attribute_df, xs_df, output_dir, in_banks):
         plt.title(f'Cross-section No. {i + 1} at LHD No. {lhd_id}')
 
         # display plot
-        png_output = output_dir + f'/Cross-section No.{i+1} at LHD No.{lhd_id}.png'
+        png_output = output_dir + f'/Cross-section No.{i+1} at LHD No. {lhd_id}.png'
         plt.savefig(png_output, dpi=300, bbox_inches='tight')
         plt.show()
 
+
+def plot_rating_curve(attribute_df, output_dir):
+    # define range of flows
+    x = np.linspace(1, 100, 100)
+
+    # initialize the plot
+    plt.figure(figsize=(10, 6))
+
+    # iterate through the rating curves
+    for index, row in attribute_df.iterrows():
+        a = row['depth_a']
+        b = row['depth_b']
+        y = a * x**b
+        plt.plot(x, y, label=f'Downstream Cross-section {row["id"]}: $y = {a:.3f} x^{{{b:.3f}}}$')
+
+    # Add labels and legend
+    plt.xlabel('Flow (m$^{3}$/s)')
+    plt.ylabel('Depth (m)')
+    plt.title(f'Downstream Rating Curves at LHD No. {lhd_id}')
+    plt.legend(title="Rating Curve Equations", loc='best', fontsize='small')  # Add legend
+    plt.grid(True)
+
+    # create output path for png file and save it
+    png_output = output_dir + f'/Downstream Rating Curves at LHD No. {lhd_id}.png'
+    plt.savefig(png_output, dpi=300, bbox_inches='tight')
+    plt.show()
+
+
 """
-Test Case: 
+this is the real deal... at least a real test case
 """
 
-test_dbf = "C:/Users/ki87ujmn/Downloads/rathcelon-example/results/28/VDT/28_Local_CurveFile.dbf"
-test_txt = "C:/Users/ki87ujmn/Downloads/rathcelon-example/results/28/XS/28_XS_Out.txt"
-test_output = 'C:/Users/ki87ujmn/Downloads'
+# this folder has the results from some example runs
+all_results = "C:/Users/ki87ujmn/Downloads/rathcelon-example/results"
+# these are the subdirectories for each rathcelon run
+rath_runs = [os.path.join(all_results, d) for d in os.listdir(all_results) if os.path.isdir(os.path.join(all_results, d))]
 
-test_att_tbl = get_attribute_df(test_dbf)
-test_df = get_xs_df(test_txt)
+for rath_run in rath_runs:
+    # the lhd_id is the name of each directory
+    lhd_id = rath_run.split('\\')[-1]
+    # the dbf and txt will be in the same place for each file
+    result_dbf = rath_run + f'/VDT/{lhd_id}_Local_CurveFile.dbf'
+    result_txt = rath_run + f'/XS/{lhd_id}_XS_Out.txt'
+    # get the attribute table from the dbf
+    attr_tbl = get_attribute_df(result_dbf)
+    # get the cross-section data from the txt
+    cross_section = get_xs_df(result_txt)
+    # plot rating curves and cross-sections
+    plot_rating_curve(attr_tbl, rath_run)
+    plot_cross_sections(attr_tbl, cross_section, rath_run, False)
 
-plot_rating_curve(test_att_tbl, test_output)
-plot_cross_sections(test_att_tbl, test_df, test_output, False)
