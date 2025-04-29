fe0d848f85e93da4d9ae4e4d7f10e4fdd1947fc7
ktquint <ktquint@byu.edu>
2025-04-29 09:56:20 -0600

    still updating CrossSection class. need to add distance down/upstream to the dbf if I want the figures to say how far they are from the dam

--- rathcelon_analysis/classes.py
+++ rathcelon_analysis/classes.py
@@ -6,6 +6,7 @@ the ID_XS_Out.txt file for each dam
 
 Dam holds the cross-section information
 """
+import ast
 import numpy as np
 import pandas as pd
 import dbfread as dbf
@@ -13,7 +14,7 @@ import matplotlib.pyplot as plt
 
 
 class CrossSection:
-    def __init__(self, lhd_id, lateral, elevation, wse, distance, location, depth_a, depth_b):
+    def __init__(self, lhd_id, index, xs_row): # lateral, elevation, wse, distance, location, depth_a, depth_b, lat, lon):
         """
         lateral is a [] of lateral distances
         elevation is a [] of elevations
@@ -25,13 +26,25 @@ class CrossSection:
         depth_a is a & depth_b is b
         """
         self.id = lhd_id
-        self.lateral = lateral
-        self.elevation = elevation
-        self.wse = wse
-        self.distance = distance
-        self.location = location
-        self.a = depth_a
-        self.b = depth_b
+        # geospatial info
+        self.lat = xs_row['Lat']
+        self.lon = xs_row['Lon']
+        if index == 4:
+            self.location = 'Upstream'
+        else:
+            self.location = 'Downstream'
+        # rating curve info
+        self.a = xs_row['depth_a']
+        self.b = xs_row['depth_b']
+        # cross-section plot info
+        self.wse = xs_row['wse_1']
+
+
+        # self.lateral = lateral
+        # self.elevation = elevation
+
+        # self.distance = distance
+
 
     def trim_to_banks(self):
         # this autofilled... i'll fit it later
@@ -85,21 +98,29 @@ class Dam:
         id_row = lhd_df[lhd_df['ID'] == self.id]
         self.latitude = id_row['Latitude'].values[0]
         self.longitude = id_row['Longitude'].values[0]
-        vdt_loc = f'{project_dir}/LHD_Results/{self.id}/VDT/{id}_Local_CurveFile.dbf'
-        xs_loc = f'{project_dir}/LHD_Results/{self.id}/XS/{id}_XS_Out.txt'
-
-
         self.cross_sections = []
-
-    def add_cross_section(self, vdt_file, cross_section_file):
-        """
-        vdt_file is the ID_Local_CurveFile.dbf file
-        cross_section_file is the ID_XS_Out.txt file
-        """
-        attribute_table = dbf.DBF(vdt_file)
-
-
-        self.cross_sections.append(cross_section)
+        # find attributes based on the vdt and xs files
+        vdt_loc = f'{project_dir}/LHD_Results/{self.id}/VDT/{self.id}_Local_CurveFile.dbf'
+        xs_loc = f'{project_dir}/LHD_Results/{self.id}/XS/{self.id}_XS_Out.txt'
+        # read in dbf then translate it to data.frame
+        vdt_dbf = dbf.DBF(vdt_loc)
+        vdt_df = pd.DataFrame(iter(vdt_dbf))
+        # read in txt as data.frame
+        xs_df = pd.read_csv(xs_loc, header=None, sep='\t')
+        xs_df.rename(columns={0: 'COMID', 1: 'Row', 2: 'Col',
+                              3: 'elev_1', 4: 'wse_1', 5: 'lat_1', 6: 'n_1',
+                              7: 'elev_2', 8: 'wse_2', 9: 'lat_2', 10: 'n_2'},
+                     inplace=True)
+        # evaluate the strings as literals (lists)
+        xs_df['elev_1'] = xs_df['elev_1'].apply(ast.literal_eval)
+        xs_df['n_1'] = xs_df['n_1'].apply(ast.literal_eval)
+        xs_df['elev_2'] = xs_df['elev_2'].apply(ast.literal_eval)
+        xs_df['n_2'] = xs_df['n_2'].apply(ast.literal_eval)
+        # let's merge the tables
+        merged_df = pd.merge(vdt_df, xs_df, on=['Row', 'Col'], how='left')
+        # let's go through each row of the df and add cross-sections to the dam.
+        for index, row in merged_df.iterrows():
+            self.cross_sections.append(CrossSection(lhd_id, index, row))
 
     def plot_rating_curves(self):
         for cross_section in self.cross_sections:
