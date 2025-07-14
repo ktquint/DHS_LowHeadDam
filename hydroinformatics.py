052ae45e7673f2b7cd6a5661eb7137f0ce6ec8c2
ktquint <ktquint@byu.edu>
2025-07-14 09:33:39 -0600

    redownloaded the project to fix the big file problems

--- hydroinformatics.py
+++ hydroinformatics.py
@@ -33,9 +33,9 @@ def haversine(lat1, lon1, lat2, lon2):
     """Computes approximate distance between two points (km)."""
     from math import radians, sin, cos, sqrt, atan2
     R = 6371.0  # Earth radius in km
-    dlat = radians(lat2 - lat1)
-    dlon = radians(lon2 - lon1)
-    a = sin(dlat/2)**2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(dlon/2)**2
+    d_lat = radians(lat2 - lat1)
+    d_lon = radians(lon2 - lon1)
+    a = sin(d_lat/2)**2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(d_lon/2)**2
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
     return R * c
 
